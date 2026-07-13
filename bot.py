3ae5a09275af4e6fb2e1af54ecb0581260c8122b
Alexxfreeman <alexxfreeman@gmail.com>
2026-07-13 14:29:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -436,7 +436,7 @@ async def callback_check_all_other(callback_query: CallbackQuery):
 
 async def main():
     await init_db()
-    await magnit_api.init_browser()  # Инициализируем браузер
+    await magnit_api.init_browser()  # ← Добавлено
     dp.message.middleware(LoggingMiddleware())
     dp.callback_query.middleware(LoggingMiddleware())
     dp.include_router(router)
